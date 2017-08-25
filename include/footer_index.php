a45d2d25e7f5fcdfa5a847bf6ca5de70e3c31614
s-nadesh <nadesh@arkinfotec.com>
2017-08-25 13:29:28 +0530

    Footer files

---
+++ include/footer_index.php
@@ -0,0 +1,3 @@
+<div class="inner-footer section section10" id="section10">
+<?php include 'footer.php' ?>
+</div>
---
+++ include/footer_inner.php
@@ -0,0 +1,5 @@
+<footer class="inner-footer">
+    <div class="section section10" id="section10">
+        <?php include 'footer.php' ?>
+    </div>
+</footer>
