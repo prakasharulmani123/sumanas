cb81f2297f8407ec3daf4c94ec116d10fbf67703
s-nadesh <nadesh@arkinfotec.com>
2017-08-25 12:29:51 +0530

    changes in head page

--- careers.php
+++ careers.php
@@ -1,31 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="css/examples.css" />
-        <!--<link rel="stylesheet" type="text/css" href="css/jquery.fullpage.css" />-->
-        <link rel="stylesheet" type="text/css" href="css/style.css" />
-        <link rel="stylesheet" type="text/css" href="css/responsive.css" />
-        <link rel="stylesheet" href="css/owl.carousel.min.css">
-        <link rel="stylesheet" href="css/owl.theme.default.min.css">
-        <link rel="stylesheet" href="css/font-awesome.css">
-        <link rel="stylesheet" href="css/metisMenu.min.css">
-        <link rel="stylesheet" href="css/jquery.mCustomScrollbar.css">
-
-        <!--<link rel="stylesheet" href="css/jquery.es-drawermenu.min.css">-->
-
-        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-        <!--[if lt IE 9]>
-              <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-              <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-            <![endif]-->
-
-        <title>Sumanas Technologies - Careers</title>
-    </head>
+<?php include 'head.php'; ?>
     <body class="innerpage">
         <?php include 'header.php' ?>
         <div class="inner-page-heading ">
