4b63be7765cb917c979d1f91197d13417b871ef4
Arivu <arivazhagan.pandi@arkinfotec.com>
2017-08-25 13:46:36 +0530

    #Deleted Old Files

--- include/footer_index.php
+++
@@ -1,3 +0,0 @@
-<div class="inner-footer section section10" id="section10">
-<?php include 'footer.php' ?>
-</div>
--- include/footer_inner.php
+++
@@ -1,5 +0,0 @@
-<footer class="inner-footer">
-    <div class="section section10" id="section10">
-        <?php include 'footer.php' ?>
-    </div>
-</footer>
